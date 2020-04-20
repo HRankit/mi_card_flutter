af50c3213a43e3eef9d21df09d8faeb465237501
Linux Developer for android <linux_dentalpockets@comsec.co.in>
2020-04-20 13:44:38 +0530

    Finished Flutter layout Challenge.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.red,
+              ),
+              SizedBox(
+                height: 20,
+                width: 40,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+                width: 40,
+              ),
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.blue,
+                child: Text(
+                  'Container 3',
+                  style: TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
