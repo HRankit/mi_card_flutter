1748a9b57b623565fbd1e75b6ce545c95db30ed0
Linux Developer for android <linux_dentalpockets@comsec.co.in>
2020-04-22 10:25:11 +0530

    Finished Lesson - Add Material Icons

--- lib/main.dart
+++ lib/main.dart
@@ -11,50 +11,84 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/NES-ROB.png'),
               ),
-              SizedBox(
-                height: 20,
-                width: 40,
+              Text(
+                'Nes - Rob',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-                width: 40,
+              Text(
+                'A STUPID ROBOT',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 6,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.blue,
-                child: Text(
-                  'Container 3',
-                  style: TextStyle(
-                    fontSize: 10,
-                  ),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+ 91 999-999-9999',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: 'Source Sans Pro',
+                        letterSpacing: 2.5,
+                      ),
+                    ),
+                  ],
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: 5,
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'this@that.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: 'Source Sans Pro',
+                        letterSpacing: 2.5,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
